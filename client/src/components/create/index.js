e48abaf220bc217ce3c36c5b54800b50084cc1b6
Lena Nevel <39684522+LenaNevel@users.noreply.github.com>
2022-12-20 08:57:23 -0600

    added the preview pannel

--- client/src/components/create/index.js
+++ client/src/components/create/index.js
@@ -1,24 +1,25 @@
 import React, { useState } from "react";
 
 import "./index.css";
-const toDo = [{id: 1, value: 'add the preview'}, {id: 2, value: 'save to database'}, {id: 3, value: 'get a store going'}, {id: 4, value: 'fix title to take on % of height'}];
-let toDoTitle = ''
+const toDo = [{ id: 1, value: 'add the preview' }, { id: 2, value: 'save to database' }, { id: 3, value: 'get a store going' }, { id: 4, value: 'fix title to take on % of height' }];
+let toDoTitle = 'ToDo List Project';
 
 const CreateToDoList = () => {
   const [toDoList, updateToDoListState] = useState(toDo);
 
   const addToList = (event) => {
-    if (event.keyCode == 13) {
+    if (event.keyCode === 13) {
       toDo.push({ id: toDo.length + 1, value: event.target.value });
       updateToDoListState([...toDo]);
     }
   };
 
+
   const editToDoList = (event) => {
     const index = toDo.findIndex(object => {
       return object.id.toString() === event.target.id;
     });
-    if (index != -1) {
+    if (index > -1) {
       toDo[index].value = event.target.value;
       updateToDoListState([...toDo]);
     }
@@ -39,25 +40,39 @@ const CreateToDoList = () => {
     });
     toDoListMap.push(
       <input
-          key={(toDoList.length + 1).toString()}
-          className="create-list-items"
-          placeholder="enter TO DO"
-          onKeyUp={addToList}
-          autoFocus
-        ></input>
+        key={(toDoList.length + 1).toString()}
+        className="create-list-items"
+        placeholder="enter TO DO"
+        onKeyUp={addToList}
+        autoFocus
+      ></input>
     )
+    return toDoListMap;
+  }
+
+  const createPreviewListMap = () => {
+    const toDoListMap = toDoList.map(function (todo) {
+      return (
+        <li
+          key={todo.id.toString()}
+          className="create-list-preview-items"
+          id={todo.id.toString()}
+        >{todo.value}</li>
+      );
+    });
     return toDoListMap;
   }
 
   const editTitleToDo = (event) => {
     toDoTitle = event.target.value;
   }
-  
-  
+
+
   return (
     <div className="create-list">
       <div className="create-list-content">
         <div className="create-list-editor">
+          <div className="create-list-editor-content">
           <div className="create-list-title-content">
             <input
               className="create-list-title"
@@ -67,8 +82,16 @@ const CreateToDoList = () => {
             />
           </div>
           <div className="create-list-items-content">{createToDoListMap()}</div>
+          </div>
         </div>
-        <div className="create-list-preview">for me and you</div>
+        <div className="create-list-preview">
+          <div className="create-list-preview-content">
+            <div className="create-list-preview-title-content">
+              <div className="create-list-preview-title">{toDoTitle}</div>
+              <ol className="create-list-preview-items-content">{createPreviewListMap()}</ol>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
