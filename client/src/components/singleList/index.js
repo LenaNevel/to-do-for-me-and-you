fb94e9823622dc7d2c55e4b08155de3ada1fd1e7
Lena Nevel <39684522+LenaNevel@users.noreply.github.com>
2022-12-28 11:58:27 -0600

    adding single list component

---
+++ client/src/components/singleList/index.js
@@ -0,0 +1,14 @@
+import './index.css';
+
+const SingleList = () => {
+    console.log('i am in singleList')
+    return (
+        <div className="dashboard">
+      <div className="dashboard-content">
+       
+      </div>
+    </div>
+    );
+};
+
+export default SingleList;
--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,29 +1,34 @@
-import "./index.css";
+import './index.css';
 import { useGetListsQuery } from '../../redux/apiSlice';
+
+import SingleList from '../../components/singleList';
 
 
 const Dashboard = () => {
 
   const {
-    data: lists = [],
-    isLoading,
-    isFetching,
+    data: lists = {},
+    // isLoading,
+    // isFetching,
     isSuccess,
-    isError,
-    error,
+    // isError,
+    // error,
   } = useGetListsQuery()
 
-  console.log(lists)
-  console.log(isSuccess)
-  console.log(isLoading)
-  console.log(isError)
+  // console.log(lists)
+  // console.log(isSuccess)
+  // console.log(isLoading)
+  // console.log(isError)
+  // console.log(isFetching)
+  // console.log(error)
 
-    return (
-      <div className="dashboard">
-        <div className="dashboard-content">
-        </div>
+  return (
+    <div className="dashboard">
+      <div className="dashboard-content">
+        {isSuccess && lists && Object.keys(lists).map((list) => <SingleList data={lists[list]} key={list}/>)}
       </div>
-    );
+    </div>
+);
 };
 
 export default Dashboard;
